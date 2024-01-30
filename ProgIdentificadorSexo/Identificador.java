4fcc085871aad7e5967ffa39fbc7197905464fe9
Thomas Henrique <thomasnhenrique@gmail.com>
2024-01-30 20:24:38 -0300

    Create Identificador.java

---
+++ ProgIdentificadorSexo/Identificador.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class Sexo {
+    public static void main(String args[]) {
+        // Declaração da variável para armazenar o sexo
+        char sexo;
+        
+        // Solicita ao usuário que insira seu sexo e converte para maiúsculo para evitar distinção entre maiúsculas e minúsculas
+        sexo = JOptionPane.showInputDialog(null, "Digite seu sexo:").toUpperCase().charAt(0);
+        
+        // Utilização do switch-case para verificar o sexo e exibir uma mensagem correspondente
+        switch (sexo) {
+            case 'M':
+                // Se o sexo for 'M', exibe mensagem indicando sexo masculino
+                JOptionPane.showMessageDialog(null, "Sexo Masculino");
+                break;
+            case 'F':
+                // Se o sexo for 'F', exibe mensagem indicando sexo feminino
+                JOptionPane.showMessageDialog(null, "Sexo Feminino");
+                break;
+            default:
+                // Se o sexo não for 'M' nem 'F', exibe mensagem indicando sexo indefinido
+                JOptionPane.showMessageDialog(null, "Sexo Indefinido");
+                break;
+        }
+    }
+}

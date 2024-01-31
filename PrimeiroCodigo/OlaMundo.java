b00ded6a1f8976448b2200bb73960f40fdaf6a65
Thomas Henrique <thomasnhenrique@gmail.com>
2024-01-31 15:03:07 -0300

    Create OlaMundo.java

---
+++ PrimeiroCodigo/OlaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * Este programa exibe uma mensagem "Olá Mundo" em uma janela de diálogo.
+ */
+
+/**
+ * A classe OlaMundo é responsável por exibir a mensagem "Olá Mundo" em uma janela de diálogo.
+ * 
+ * @author thoma
+ */
+import javax.swing.*;
+
+public class OlaMundo { // Início da classe OlaMundo
+    
+    public static void main(String args[]) { // Início do método principal
+        
+        // Exibe a mensagem "Olá Mundo" em uma janela de diálogo
+        JOptionPane.showMessageDialog(null, "Olá Mundo");
+        
+    } // Fim do método principal
+    
+} // Fim da classe OlaMundo

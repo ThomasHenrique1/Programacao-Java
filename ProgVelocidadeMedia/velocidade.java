0e405ec5180d55d36d82b75c993436056d7fef71
Thomas Henrique <thomasnhenrique@gmail.com>
2024-01-31 15:08:04 -0300

    Update velocidade.java

--- ProgVelocidadeMedia/velocidade.java
+++ ProgVelocidadeMedia/velocidade.java
@@ -1,36 +1,36 @@
 import java.util.Scanner;
 
-public class exercicio2 {
+public class velocidade {
     public static void main(String args[]) {
-        // Criação do objeto Scanner para receber entrada do usuário
-        Scanner scanner = new Scanner(System.in);
-        
-        // Declaração das variáveis para armazenar a distância, tempo e velocidade média
-        float distancia, tempo, vmedia;
-        
-        // Solicita ao usuário que insira a distância percorrida pelo piloto em km
-        System.out.println("Digite a distância percorrida pelo piloto em km: ");
-        distancia = scanner.nextFloat();
-        
-        // Solicita ao usuário que insira o tempo transcorrido em horas
-        System.out.println("Digite o tempo transcorrido, em hora: ");
-        tempo = scanner.nextFloat();
-        
-        // Calcula a velocidade média
-        vmedia = distancia / tempo;
-        
-        // Verifica se o tempo e a distância são valores positivos
-        if (tempo >= 0 && distancia >= 0) {
-            // Se ambos forem positivos, exibe a velocidade média
-            System.out.println("Sua velocidade média final foi de: " + vmedia + " Km/h");
-        } else if (tempo <= 0) {
-            // Se o tempo for negativo ou zero, exibe uma mensagem de erro relacionada ao tempo
-            System.out.println("OPS!\nErro 404, o valor de tempo deve ser positivo");
-            System.out.println("Tente novamente ");
-        } else {
-            // Se a distância for negativa ou zero, exibe uma mensagem de erro relacionada à distância
-            System.out.println("OPS!\nErro 404, o valor percorrido deve ser positivo");
-            System.out.println("Tente novamente !");
+        try (// Criação do objeto Scanner para receber entrada do usuário
+        Scanner scanner = new Scanner(System.in)) {
+            // Declaração das variáveis para armazenar a distância, tempo e velocidade média
+            float distancia, tempo, vmedia;
+            
+            // Solicita ao usuário que insira a distância percorrida pelo piloto em km
+            System.out.println("Digite a distância percorrida pelo piloto em km: ");
+            distancia = scanner.nextFloat();
+            
+            // Solicita ao usuário que insira o tempo transcorrido em horas
+            System.out.println("Digite o tempo transcorrido, em hora: ");
+            tempo = scanner.nextFloat();
+            
+            // Calcula a velocidade média
+            vmedia = distancia / tempo;
+            
+            // Verifica se o tempo e a distância são valores positivos
+            if (tempo >= 0 && distancia >= 0) {
+                // Se ambos forem positivos, exibe a velocidade média
+                System.out.println("Sua velocidade média final foi de: " + vmedia + " Km/h");
+            } else if (tempo <= 0) {
+                // Se o tempo for negativo ou zero, exibe uma mensagem de erro relacionada ao tempo
+                System.out.println("OPS!\nErro 404, o valor de tempo deve ser positivo");
+                System.out.println("Tente novamente ");
+            } else {
+                // Se a distância for negativa ou zero, exibe uma mensagem de erro relacionada à distância
+                System.out.println("OPS!\nErro 404, o valor percorrido deve ser positivo");
+                System.out.println("Tente novamente !");
+            }
         }
     }
 }

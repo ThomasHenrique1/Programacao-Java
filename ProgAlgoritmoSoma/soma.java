3772e0388a47bb458bb6cfae98ad5572d8fb6eec
Thomas Henrique <thomasnhenrique@gmail.com>
2024-01-31 15:07:16 -0300

    Update soma.java

--- ProgAlgoritmoSoma/soma.java
+++ ProgAlgoritmoSoma/soma.java
@@ -1,28 +1,29 @@
 import java.util.Scanner;
 
-public class Algoritmo_soma_10 {
+public class soma {
     public static void main(String args[]) {
-        // Criação do objeto Scanner para receber entrada do usuário
-        Scanner scanner = new Scanner(System.in);
-        
-        // Declaração das variáveis para armazenar os números e a soma
-        float n1, n2, soma;
-        
-        // Solicita ao usuário que insira o primeiro número
-        System.out.println("Digite um número:");
-        n1 = scanner.nextFloat();
-        
-        // Solicita ao usuário que insira o segundo número
-        System.out.println("Digite outro número:");
-        n2 = scanner.nextFloat();
-        
-        // Calcula a soma dos números
-        soma = n1 + n2;
-        
-        // Verifica se a soma é maior que 10
-        if (soma > 10) {
-            // Se a soma for maior que 10, exibe a soma
-            System.out.println("A soma é " + soma);
+        // Inicialização do Scanner para receber entrada do usuário
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Declaração das variáveis para armazenar os números e a soma
+            float n1, n2, soma;
+            
+            // Solicita ao usuário que insira o primeiro número
+            System.out.println("Digite um número:");
+            n1 = scanner.nextFloat();
+            
+            // Solicita ao usuário que insira o segundo número
+            System.out.println("Digite outro número:");
+            n2 = scanner.nextFloat();
+            
+            // Calcula a soma dos números
+            soma = n1 + n2;
+            
+            // Verifica se a soma é maior que 10
+            if (soma > 10) {
+                // Se a soma for maior que 10, exibe a soma
+                System.out.println("A soma é " + soma);
+            }
         }
     }
 }
+

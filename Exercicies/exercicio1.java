62937cf10f528ad0674967a50bceb7093d61e4a2
Thomas Henrique <thomasnhenrique@gmail.com>
2024-01-31 15:14:44 -0300

    Update exercicio1.java

--- Exercicies/exercicio1.java
+++ Exercicies/exercicio1.java
@@ -8,21 +8,21 @@ public class exercicio1 {
         
         acum = 0; // Inicializa a soma acumulada como 0
         
-        // Criação do objeto Scanner para receber entrada do usuário
-        Scanner scanner = new Scanner(System.in);
-        
-        // Solicita ao usuário que insira a quantidade de valores que deseja processar
-        System.out.println("Quantos valores deseja processar?");
-        qtdValores = scanner.nextInt();
-        
-        // Loop para receber os valores e acumular a soma
-        for (i = 1; i <= qtdValores; i++) {
-            // Solicita ao usuário que insira um valor
-            System.out.println("Digite o valor " + i + ":");
-            valor = scanner.nextFloat();
+        try (// Criação do objeto Scanner para receber entrada do usuário
+        Scanner scanner = new Scanner(System.in)) {
+            // Solicita ao usuário que insira a quantidade de valores que deseja processar
+            System.out.println("Quantos valores deseja processar?");
+            qtdValores = scanner.nextInt();
             
-            // Acumula a soma dos valores inseridos
-            acum = acum + valor;
+            // Loop para receber os valores e acumular a soma
+            for (i = 1; i <= qtdValores; i++) {
+                // Solicita ao usuário que insira um valor
+                System.out.println("Digite o valor " + i + ":");
+                valor = scanner.nextFloat();
+                
+                // Acumula a soma dos valores inseridos
+                acum = acum + valor;
+            }
         }
         
         // Calcula a média dos valores inseridos

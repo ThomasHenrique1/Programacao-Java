c4daf34b4d6f015b9a5d249c854665c0682a7bc7
Thomas Henrique <thomasnhenrique@gmail.com>
2024-02-01 19:41:41 -0300

    Ordenacao

---
+++ ProjetoAula8/company/Exerc01.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Exerc01 {
+
+    public static void main(String[] args) {
+        double vetor[]= {4.9,6.1,0.1,1.9,2.4,7.2,5.5};
+        double v[] = vetor;
+        int i;
+        double a = 0;
+        int f= v.length -1;
+        for(i=0;i<f;i++){
+            a = v[i];
+            v[i]= v[f];
+            v[f] = a;
+            f--;
+
+
+        }
+        int b;
+        for(b = 0;b<v.length;b++){
+            System.out.println(v[b]);
+        }
+    }
+}
